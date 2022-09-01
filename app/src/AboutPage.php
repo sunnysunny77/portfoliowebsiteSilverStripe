751a558f39954177776aa795e02f95ab4ba36e39
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-09-01 12:37:55 +0800

    Update AboutPage.php

--- app/src/AboutPage.php
+++ app/src/AboutPage.php
@@ -6,5 +6,18 @@ use Page;
 class AboutPage extends Page
 {
 
-   
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+
+        $fields->removeByName('Title');
+
+        $fields->removeByName('URLSegment');
+
+        $fields->removeByName('MenuTitle');
+
+        $fields->removeByName('Content');
+
+        return $fields;
+    }
 }
