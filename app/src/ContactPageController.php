c03e7b1200d108ee666d838f6a4727f1ddbcf30e
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-09-03 00:54:41 +0800

    Update ContactPageController.php

--- app/src/ContactPageController.php
+++ app/src/ContactPageController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace pages;
 
 use PageController;
@@ -11,44 +10,51 @@ use SilverStripe\Forms\TextareaField;
 use SilverStripe\Forms\FormAction;
 use SilverStripe\Forms\Form;
 use SilverStripe\Forms\RequiredFields;
-use SilverStripe\Control\Email\Email;
-
+use SilverStripe\Forms\HeaderField;
 
 class ContactPageController extends PageController
 {
 
-  
     private static $allowed_actions = ['HireForm'];
 
     public function HireForm() 
     { 
+
         $form = Form::create(
             $this,
             __FUNCTION__,
             FieldList::create(
+                HeaderField::create('Header', 'Hire')
+                ->addExtraClass('lead'),
+        
+
                 TextField::create('first-name','First name')
                 ->setAttribute('autocomplete', 'on')
                 ->setAttribute('maxlength', '20'),
+
                 TextField::create('last-name','Last name')
                 ->setAttribute('autocomplete', 'on')
                 ->setAttribute('maxlength', '20'),  
+
                 EmailField::create('email','Email')
                 ->setAttribute('autocomplete', 'on')
                 ->setAttribute('maxlength', '20'),
+
                 TextField::create('phone','Phone')
                 ->setAttribute('autocomplete', 'on')
                 ->setAttribute('pattern', '[+]?[0-9]{3,15}')
                 ->setAttribute('title', 'Accepts +###############'),
+
                 TextareaField::create('text','Position description')
                 ->setAttribute('maxlength', '1000')
             ),
             FieldList::create(
+
                 FormAction::create('handleHire', 'Submit')
                     ->addExtraClass('button')
             ),
             RequiredFields::create('first-name', 'last-name', 'email','phone', 'text')
         );
-
      
         $data = $this->getRequest()->getSession()->get("FormData.{$form->getName()}.data");
 
@@ -57,24 +63,35 @@ class ContactPageController extends PageController
 
     public function handleHire($data, $form) 
     { 
-        $email = new Email(); 
-         
-        $email->setTo('shlooby07@gmail.com'); 
-        $email->setFrom($data['email']); 
-        $email->setSubject("Hire Message from {$data["first-name"]}"); 
-         
-        $messageBody = " 
-            <p><strong>Name:</strong> {$data['first-name']} {$data['last-name']}</p> 
-            <p><strong>Email:</strong> {$data['email']} </p> 
-            <p><strong>Phone:</strong> {$data['phone']} </p> 
-            <p><strong>Position description:</strong> {$data['text']}</p> 
-        "; 
-        $email->setBody($messageBody); 
-        $email->send(); 
-        return [
-            'Content' => '<p>Thank you for your feedback.</p>',
-            'Form' => ''
-        ];
+
+        $session = $this->getRequest()->getSession();
+        $session->set("FormData.{$form->getName()}.data", $data);
+
+        $first_name = $data["first-name"];
+        $last_name = $data["last-name"];
+        $email = $data["email"];
+        $phone = $data["phone"];
+        $text = $data["text"];
+        $to_email = "shlooby07@gmail.com";
+        $subject = "New Hire Form Message";
+
+        $contactus = "
+        You have a message from the Hire Form on your website:
+        Name: ".$first_name." ".$last_name."
+        Email: ".$email."
+        Phone: ".$phone."
+        Position description: ".$text;
+        $contactus  = wordwrap($contactus ,70);
+
+        $mail = mail($to_email,$subject,$contactus);
+
+        if (!$mail) {
+            $form->sessionMessage('Erros sending', 'bad');
+          } else {
+            $form->sessionMessage('Email sent', 'good');
+          }
+
+        return $this->redirectBack();
     }
     
     protected function init()
