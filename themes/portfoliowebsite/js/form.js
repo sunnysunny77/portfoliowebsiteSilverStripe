45e36e0a31f9bb2d605de6b386c37f710a39c67d
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-09-04 15:01:06 +0800

    Update form.js

--- themes/portfoliowebsite/js/form.js
+++ themes/portfoliowebsite/js/form.js
@@ -1,34 +1,46 @@
-$(document).ready(function() { 
-  
-  const searchParams = new URLSearchParams(window.location.search)
-  const active = searchParams.get('active') 
+$(document).ready(function () {
 
-  if (active === "1")  {
+  const searchParams = new URLSearchParams(window.location.search)
+  const active = searchParams.get('active')
+
+  if (active === "1") {
     return
   } else if (active === "2") {
-     
-      $('#isOne').removeClass("is-active");
-      $('#isOneA').attr("tabindex",-1);
-      $('#isOneA').attr("aria-selected",false);
-      $('#isTwoA').attr("tabindex",0);
-      $('#isTwoA').attr("aria-selected",true);
-      $('#isTwo').addClass("is-active");
-      $('#panel1').attr("aria-hidden",true);
-      $('#panel1').removeClass("is-active");
-      $('#panel2').addClass("is-active");
-      $('#panel2').removeAttr("aria-hidden");
-  
+
+    const one = $('#isOne')
+    const oneA = $('#isOneA')
+    const panel1 = $('#panel1')
+    const isTwoA = $('#isTwoA')
+    const panel2 = $('#panel2')
+
+    one.removeClass("is-active");
+    oneA.attr("tabindex", -1);
+    oneA.attr("aria-selected", false);
+    isTwoA.attr("tabindex", 0);
+    isTwoA.attr("aria-selected", true);
+    $('#isTwo').addClass("is-active");
+    panel1.attr("aria-hidden", true);
+    panel1.removeClass("is-active");
+    panel2.addClass("is-active");
+    panel2.removeAttr("aria-hidden");
+
   } else if (active === "3") {
 
-    $('#isOne').removeClass("is-active");
-    $('#isOneA').attr("tabindex",-1);
-    $('#isOneA').attr("aria-selected",false);
-    $('#isThreeA').attr("tabindex",0);
-    $('#isThreeA').attr("aria-selected",true);
+    const one = $('#isOne')
+    const oneA = $('#isOneA')
+    const panel1 = $('#panel1')
+    const isThreeA = $('#isThreeA')
+    const panel3 = $('#panel3')
+
+    one.removeClass("is-active");
+    oneA.attr("tabindex", -1);
+    oneA.attr("aria-selected", false);
+    isThreeA.attr("tabindex", 0);
+    isThreeA.attr("aria-selected", true);
     $('#isThree').addClass("is-active");
-    $('#panel1').attr("aria-hidden",true);
-    $('#panel1').removeClass("is-active");
-    $('#panel3').addClass("is-active");
-    $('#panel3').removeAttr("aria-hidden");
+    panel1.attr("aria-hidden", true);
+    panel1.removeClass("is-active");
+    panel3.addClass("is-active");
+    panel3.removeAttr("aria-hidden");
   }
 })
